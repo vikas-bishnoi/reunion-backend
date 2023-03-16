478512eca8eba0b11c92fe446818946912f0d483
vikaskbm <vikaskbm@gmail.com>
2023-03-16 23:11:20 +0530

    login a user and return JWT token

--- main/views.py
+++ main/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.conf import settings
+
+import jwt
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(["POST"])
+def login(request):
+    email = request.data.get("email")
+    password = request.data.get("password")
+
+    if not email or not password:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    user = authenticate(email=email, password=password)
+    print(user)
+    if user:
+        encoded_jwt = jwt.encode(
+            {"id": user.pk},
+            settings.SECRET_KEY,
+            algorithm="HS256"
+        )
+        return Response(data={"token": encoded_jwt}, status=status.HTTP_202_ACCEPTED)
+    else:
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
--- reunion/settings.py
+++ reunion/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'main.User'
+
+SECRET_KEY = 'SflKxwRJSMeKKJf36PPOkM6POkMeeKKJ6ssw5c'

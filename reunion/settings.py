4dca91f0f3edbd36981448d901388e7455008e4e
vikaskbm <vikaskbm@gmail.com>
2023-03-17 19:51:39 +0530

    Add gunicorn dependency

--- reunion/settings.py
+++ reunion/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-(8p8tihl85$@s2dw#w=g%r_v6(@kr!^qoh%-u9(hjc@qymde94
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

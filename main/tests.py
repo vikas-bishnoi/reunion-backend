e475aff6bee093ec8634f6717c1fffa22b1e31f3
vikaskbm <vikaskbm@gmail.com>
2023-03-17 20:11:16 +0530

    Setup user tests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your tests here.
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+REGISTER_URL = reverse('main:register')
+AUTHENTICATE_URL = reverse('main:authenticate')
+USER_URL = reverse('main:user')
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserAPITests(TestCase):
+    """these tests would not require authernications"""
+
+    def setUp(self):
+        self.client = APIClient()
+
--- main/urls.py
+++ main/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path("authenticate/", views.login),
-    path("user/", views.UserView.as_view()),
-    path("register/", views.RegisterUserView.as_view()),
-    path("follow/<int:pk>/", views.FollowView.as_view()),
-    path("unfollow/<int:pk>/", views.UnfollowView.as_view()),
+    path("authenticate/", views.login, name='authenticate'),
+    path("user/", views.UserView.as_view(), name='user'),
+    path("register/", views.RegisterUserView.as_view(), name='register'),
+    path("follow/<int:pk>/", views.FollowView.as_view(), name='follow'),
+    path("unfollow/<int:pk>/", views.UnfollowView.as_view(), name='unfollow'),
 ]
